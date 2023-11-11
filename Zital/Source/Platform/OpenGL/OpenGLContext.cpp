e411c38b1d211ddb23b0a8e7d3aa4c728272f76c
Dylan Williams <dylanw1502@hotmail.com>
2023-11-10 22:33:22 -0800

    Added gpu logging to se Vendor, Renderer, and Version.

--- Zital/Source/Platform/OpenGL/OpenGLContext.cpp
+++ Zital/Source/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Zital
 		glfwMakeContextCurrent(mWindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ZT_CORE_ASSERT(status, "Failed to initialize Glad.");
+
+		ZT_CORE_INFO("OpenGL Info:");
+		ZT_CORE_INFO("  Vendor: {0}", (char*)glGetString(GL_VENDOR));
+		ZT_CORE_INFO("  Renderer: {0}", (char*)glGetString(GL_RENDERER));
+		ZT_CORE_INFO("  Version: {0}", (char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
--- Zital/Source/Zital/Application.cpp
+++ Zital/Source/Zital/Application.cpp
@@ -24,6 +24,32 @@ namespace Zital
 
 		mImGuiLayer = new ImGuiLayer();
 		PushOverlay(mImGuiLayer);
+
+		glGenVertexArrays(1, &mVertexArray);
+		glBindVertexArray(mVertexArray);
+
+		glGenBuffers(1, &mVertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, mVertexBuffer);
+
+		float vertices[3 * 3] =
+		{
+			-0.5f, -0.5f, 0.f,
+			0.5f, -0.5f, 0.f,
+			0.f, 0.5f, 0.f
+		};
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 3, nullptr);
+
+		glGenBuffers(1, &mIndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIndexBuffer);
+
+		unsigned indices[3] = { 0, 1, 2 };
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	}
 
 	Application::~Application()
@@ -60,7 +86,11 @@ namespace Zital
 	{
 		while (mRunning)
 		{
+			glClearColor(0.1f, 0.1f, 0.1f, 1.f);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			glBindVertexArray(mVertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			for (Layer* layer : mLayerStack)
 				layer->OnUpdate();
